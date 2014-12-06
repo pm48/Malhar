f0ec7ed297ef7830e5b42d9c8d5dbfc8d26db216
Prerna Manaktala <prerna@datatorrent.com>
2014-12-05 16:39:29 -0800

    support added for string.

--- benchmark/src/main/java/com/datatorrent/benchmark/HiveInsertBenchmarkingApp.java
+++ benchmark/src/main/java/com/datatorrent/benchmark/HiveInsertBenchmarkingApp.java
@@ -68,7 +68,7 @@ public class HiveInsertBenchmarkingApp implements StreamingApplication
   {
     hiveStore.connect();
     Statement stmt = hiveStore.getConnection().createStatement();
-    stmt.execute("CREATE TABLE IF NOT EXISTS " + tablename + " (col1 int) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\n'  \n"
+    stmt.execute("CREATE TABLE IF NOT EXISTS " + tablename + " (col1 string) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\n'  \n"
             + "STORED AS TEXTFILE ");
     hiveStore.disconnect();
   }
