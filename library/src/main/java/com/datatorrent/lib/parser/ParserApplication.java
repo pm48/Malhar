39d2f2de3ef96c4271996496649114cfabb645ac
Prerna Manaktala <prerna@datatorrent.com>
2015-02-23 22:15:05 -0800

    changes for parser.

--- library/src/main/java/com/datatorrent/lib/parser/ParserApplication.java
+++ library/src/main/java/com/datatorrent/lib/parser/ParserApplication.java
@@ -15,8 +15,48 @@
  */
 package com.datatorrent.lib.parser;
 
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.contrib.kafka.AbstractKafkaSinglePortInputOperator;
+import org.apache.hadoop.conf.Configuration;
+import java.nio.ByteBuffer;
+import kafka.message.Message;
 
-public class ParserApplication
+public class ParserApplication implements StreamingApplication
 {
+
+  @Override
+  public void populateDAG(DAG dag, Configuration conf)
+  {
+    dag.setAttribute(DAG.STREAMING_WINDOW_SIZE_MILLIS, 1000);
+
+    KafkaSinglePortStringInputOperator kafkaStringInput = dag.addOperator("KafkaStringInput", KafkaSinglePortStringInputOperator.class);
+    Parser parser = dag.addOperator("Parser", Parser.class);
+
+    dag.addStream("Kafka2Parser", kafkaStringInput.outputPort, parser.input);
+  }
+
+  public class KafkaSinglePortStringInputOperator extends AbstractKafkaSinglePortInputOperator<byte[]>
+  {
+
+    /**
+     * Implement abstract method of AbstractActiveMQSinglePortInputOperator
+     */
+    @Override
+    public byte[] getTuple(Message message)
+    {
+      byte[] bytes = null;
+      try {
+        ByteBuffer buffer = message.payload();
+        bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+      }
+      catch (Exception ex) {
+        return bytes;
+      }
+      return bytes;
+    }
+
+  }
 
 }
