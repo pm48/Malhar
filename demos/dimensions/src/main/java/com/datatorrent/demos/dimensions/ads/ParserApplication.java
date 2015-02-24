b382ae4c7731384eaec4a7406872d656b8cef1ab
Prerna Manaktala <prerna@datatorrent.com>
2015-02-23 22:39:36 -0800

    moved ParserApplication.

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/ParserApplication.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/ads/ParserApplication.java
@@ -13,11 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.datatorrent.lib.parser;
+package com.datatorrent.demos.dimensions.ads;
 
 import com.datatorrent.api.DAG;
 import com.datatorrent.api.StreamingApplication;
 import com.datatorrent.contrib.kafka.AbstractKafkaSinglePortInputOperator;
+import com.datatorrent.lib.parser.Parser;
 import org.apache.hadoop.conf.Configuration;
 import java.nio.ByteBuffer;
 import kafka.message.Message;
