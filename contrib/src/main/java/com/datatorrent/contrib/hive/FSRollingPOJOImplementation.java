6eb205285d08b4a5da4734a83f26ace73c037501
Prerna Manaktala <prerna@datatorrent.com>
2015-05-06 15:13:03 -0700

    changes for fs rolling.

--- contrib/src/main/java/com/datatorrent/contrib/hive/FSRollingPOJOImplementation.java
+++ contrib/src/main/java/com/datatorrent/contrib/hive/FSRollingPOJOImplementation.java
@@ -16,7 +16,15 @@
 package com.datatorrent.contrib.hive;
 
 import com.datatorrent.lib.util.PojoUtils;
+import com.datatorrent.lib.util.PojoUtils.GetterBoolean;
+import com.datatorrent.lib.util.PojoUtils.GetterChar;
+import com.datatorrent.lib.util.PojoUtils.GetterDouble;
+import com.datatorrent.lib.util.PojoUtils.GetterFloat;
+import com.datatorrent.lib.util.PojoUtils.GetterInt;
+import com.datatorrent.lib.util.PojoUtils.GetterLong;
 import com.datatorrent.lib.util.PojoUtils.GetterObject;
+import com.datatorrent.lib.util.PojoUtils.GetterShort;
+import com.datatorrent.lib.util.PojoUtils.GetterString;
 import java.util.ArrayList;
 
 /*
@@ -25,37 +33,63 @@ import java.util.ArrayList;
  */
 public  class FSRollingPOJOImplementation extends AbstractFSRollingOutputOperator<Object>
 {
-  private ArrayList<String> hivePartitionColumnValues;
-  private String expression;
-  private GetterObject getter;
-  private boolean isfirstTuple = true;
+  private ArrayList<String> hivePartitionColumns;
+  private ArrayList<FIELD_TYPE> hivePartitionColumnsDataTypes;
+
+  private ArrayList<String> expression;
+  private transient ArrayList<Object> getters;
+  private transient boolean isFirstTuple;
+
+  public FSRollingPOJOImplementation()
+  {
+    super();
+    isFirstTuple = true;
+    getters = new ArrayList<Object>();
+  }
+
+  public enum FIELD_TYPE
+  {
+    BOOLEAN, DOUBLE, INTEGER, FLOAT, LONG, SHORT, CHARACTER, STRING, DATE, TIMESTAMP, OTHER
+  };
 
   /*
-   * A Java expression that will yield the specific column in hive table from the input POJO.
+   * A Java expression that will yield the specific partition column in hive table from the input POJO.
    */
-  public String getExpression()
+  public ArrayList<String> getExpression()
   {
     return expression;
   }
 
-  public void setExpression(String expression)
+  public void setExpression(ArrayList<String> expression)
   {
     this.expression = expression;
   }
 
   /*
-   * Values for Partition Columns in Hive table.
-   * Example: If partition columns are date and country, then values can be 2015-12-12,USA
-   * OR 2015-12-12,UK etc.
+   * Partition Columns in Hive table.Example: dt for date,ts for timestamp
    */
-  public ArrayList<String> getHivePartitionColumnValues()
+  public ArrayList<String> getHivePartitionColumns()
   {
-    return hivePartitionColumnValues;
+    return hivePartitionColumns;
   }
 
-  public void setHivePartitionColumnsValues(ArrayList<String> hivePartitionColumnsValues)
+  public void setHivePartitionColumnsValues(ArrayList<String> hivePartitionColumns)
   {
-    this.hivePartitionColumnValues = hivePartitionColumnsValues;
+    this.hivePartitionColumns = hivePartitionColumns;
+  }
+
+  /*
+   * Data Types of Partition Columns in Hive table.Example: date for dt column,timestamp for ts column.
+   * Particular Data Type can be chosen from the List of data types provided.
+   */
+  public ArrayList<FIELD_TYPE> getHivePartitionColumnsDataTypes()
+  {
+    return hivePartitionColumnsDataTypes;
+  }
+
+  public void setHivePartitionColumnsDataTypes(ArrayList<FIELD_TYPE> hivePartitionColumnsDataTypes)
+  {
+    this.hivePartitionColumnsDataTypes = hivePartitionColumnsDataTypes;
   }
 
   @Override
@@ -68,16 +102,69 @@ public  class FSRollingPOJOImplementation extends AbstractFSRollingOutputOperato
     return hivePartitionColumnValues;
   }
 
-  @Override
-  protected void processTuple(Object tuple)
+ @Override
+  public void processTuple(Object tuple)
   {
-    if(isfirstTuple)
-    {
-      Class<?> fqcn = tuple.getClass();
-      getter = PojoUtils.createGetterObject(fqcn, expression);
+    if (isFirstTuple) {
+      processFirstTuple(tuple);
     }
-    isfirstTuple= false;
+    isFirstTuple = false;
     super.processTuple(tuple);
+  }
+
+  public void processFirstTuple(Object tuple)
+  {
+    Class<?> fqcn = tuple.getClass();
+    int size = hivePartitionColumns.size();
+    for (int i = 0; i < size; i++) {
+      FIELD_TYPE type = hivePartitionColumnsDataTypes.get(i);
+      String getterExpression = expression.get(i);
+      if (type == FIELD_TYPE.CHARACTER) {
+        GetterChar getChar = PojoUtils.createGetterChar(fqcn, getterExpression);
+        getters.add(getChar);
+      }
+      else if (type == FIELD_TYPE.STRING) {
+        GetterString getString = PojoUtils.createGetterString(fqcn, getterExpression);
+        getters.add(getString);
+      }
+      else if (type == FIELD_TYPE.BOOLEAN) {
+        GetterBoolean getBoolean = PojoUtils.createGetterBoolean(fqcn, getterExpression);
+        getters.add(getBoolean);
+      }
+      else if (type == FIELD_TYPE.SHORT) {
+        GetterShort getShort = PojoUtils.createGetterShort(fqcn, getterExpression);
+        getters.add(getShort);
+      }
+      else if (type == FIELD_TYPE.INTEGER) {
+        GetterInt getInt = PojoUtils.createGetterInt(fqcn, getterExpression);
+        getters.add(getInt);
+      }
+      else if (type == FIELD_TYPE.LONG) {
+        GetterLong getLong = PojoUtils.createExpressionGetterLong(fqcn, getterExpression);
+        getters.add(getLong);
+      }
+      else if (type == FIELD_TYPE.FLOAT) {
+        GetterFloat getFloat = PojoUtils.createGetterFloat(fqcn, getterExpression);
+        getters.add(getFloat);
+      }
+      else if (type == FIELD_TYPE.DOUBLE) {
+        GetterDouble getDouble = PojoUtils.createGetterDouble(fqcn, getterExpression);
+        getters.add(getDouble);
+      }
+      else if (type == FIELD_TYPE.DATE) {
+        GetterObject getObject = PojoUtils.createGetterObject(fqcn, getterExpression);
+        getters.add(getObject);
+      }
+      else if (type == FIELD_TYPE.TIMESTAMP) {
+        GetterObject getObject = PojoUtils.createGetterObject(fqcn, getterExpression);
+        getters.add(getObject);
+      }
+      else if (type == FIELD_TYPE.OTHER) {
+        GetterObject getObject = PojoUtils.createGetterObject(fqcn, getterExpression);
+        getters.add(getObject);
+      }
+
+    }
   }
 
 
@@ -86,7 +173,7 @@ public  class FSRollingPOJOImplementation extends AbstractFSRollingOutputOperato
   protected byte[] getBytesForTuple(Object tuple)
   {
     //return (tuple + "\n").getBytes();
-    return (getter.get(tuple).toString() + "\n").getBytes();
+    return (getters.get(tuple).toString() + "\n").getBytes();
   }
 
 }
